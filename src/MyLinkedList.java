a758873a21f9c2520a3adcbcbc61007c2ea054d2
Aleksey Kostyunin <kostunin-ag@mail.ru>
2023-12-26 19:45:48 +0500

    Добавил решение домашнего задания

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,2 +1,84 @@
-package PACKAGE_NAME;public class MyLinkedList {
+import java.util.LinkedList;
+
+public class MyLinkedList<E> {
+
+    int size = 0; // размер
+    MyLinkedList.Node<E> first; // ссылка на первый элемент списка
+    MyLinkedList.Node<E> last; // ссылка на второй элементы списка
+
+    public void addFirst(E e) { // добавление элемента в начало списка
+        final MyLinkedList.Node<E> f = first;
+        final MyLinkedList.Node<E> newNode = new MyLinkedList.Node<>(null, e, f);
+        first = newNode;
+        if (f == null)
+            last = newNode;
+        else
+            f.prev = newNode;
+        size++;
+    }
+
+    public void addLast(E e) { // добавление элемента в конец списка
+        final MyLinkedList.Node<E> l = last;
+        final MyLinkedList.Node<E> newNode = new MyLinkedList.Node<>(l, e, null);
+        last = newNode;
+        if (l == null)
+            first = newNode;
+        else
+            l.next = newNode;
+        size++;
+    }
+
+    // получения размера списка
+    public int size() {
+        return size;
+    }
+
+    // получения элемента по индексу.
+    public E getOfIndex(int index) {
+        if (!isElementIndex(index))
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+        return node(index).item;
+    }
+
+    private boolean isElementIndex(int index) {
+        return index >= 0 && index < size;
+    }
+
+    private String outOfBoundsMsg(int index) {
+        return "Index: "+index+", Size: "+size;
+    }
+    MyLinkedList.Node<E> node(int index) {
+        if (index < (size >> 1)) {
+            MyLinkedList.Node<E> x = first;
+            for (int i = 0; i < index; i++)
+                x = x.next;
+            return x;
+        } else {
+            MyLinkedList.Node<E> x = last;
+            for (int i = size - 1; i > index; i--)
+                x = x.prev;
+            return x;
+        }
+    }
+
+    private static class Node<E> {
+        E item;
+        MyLinkedList.Node<E> next;
+        MyLinkedList.Node<E> prev;
+
+        Node(MyLinkedList.Node<E> prev, E element, MyLinkedList.Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "MyLinkedList{" +
+                "size=" + size +
+                ", first=" + first +
+                ", last=" + last +
+                '}';
+    }
 }
--- src/MyList.java
+++ src/MyList.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public interface MyList {
+public interface MyList<E>{
+
+    void addFirst(E element); // вставка элемента в начало
+    void addLast(E element); // вставка элемента в конец списка
+    int size(); // получение размера списка
+    E getOfIndex(int index); // Получение элемента по индексу
 }
